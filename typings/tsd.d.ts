2b10a2ba23ae34896a8d747a81044e90db062f9d
vachavarro <victor.chavarro@bizagi.com>
2016-01-05 11:05:38 -0500

    jquery toast conflict with angular typings fixed
    jquery toast conflict with angular typings fixed

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,5 +1,4 @@
-
 /// <reference path="angular2/angular2.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
 /// <reference path="toastr/toastr.d.ts" />
