e5d48c93d43b906c9461514d54899212e5e9e9e0
vachavarro <victor.chavarro@bizagi.com>
2016-01-06 12:00:19 -0500

    Header complete
    header complete, now the dom load after the static files

--- app/boot.ts
+++ app/boot.ts
@@ -1,1 +1,8 @@
-import "./assets/styles/talker/main.scss";
+import {bootstrap} from "angular2/platform/browser";
+import App from "./modules/home/home.component";
+
+
+bootstrap(App, []).then(
+	success => console.log(`Bizagi cloud load success`),
+	error => console.error(error)
+);
---
+++ app/modules/home/home.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "angular2/core";
+import "../../assets/styles/talker/main.scss";
+
+@Component({
+	selector: "app",
+	template: require("./home.component.html")
+})
+
+export default class App { }
